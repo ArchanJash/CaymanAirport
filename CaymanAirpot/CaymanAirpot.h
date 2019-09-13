8ce299a7017821ebebb3c20d03fcfb377cceb8d0
Archan <archan@klizos.com>
2019-09-13 12:49:31 +0530

    Initial project setup

---
+++ CaymanAirpot/CaymanAirpot.h
@@ -0,0 +1,19 @@
+//
+//  CaymanAirpot.h
+//  CaymanAirpot
+//
+//  Created by iOS Development on 9/13/19.
+//  Copyright © 2019 iOS Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CaymanAirpot.
+FOUNDATION_EXPORT double CaymanAirpotVersionNumber;
+
+//! Project version string for CaymanAirpot.
+FOUNDATION_EXPORT const unsigned char CaymanAirpotVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CaymanAirpot/PublicHeader.h>
+
+
